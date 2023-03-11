ce291c725b85ed9e42144bb6986834180e5d3339
JASON_LUI <Jason53lui@gmail.com>
2023-03-11 11:24:44 -0800

    Added the getter for the user response and sending back the AI's response data to frontend

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 import os
 import openai
@@ -21,24 +21,24 @@ def home():
 
 #list of behavoural_questions
 
-behavioural_questions = []
+behavioural_questions = ["Tell me about a time when you were not able to meet a time commitment. What prevented you from meeting it? What was the outcome and what did you learn from it?",
+                         "Describe a long-term project that you managed. How did you keep everything moving along in a timely manner?",
+                         "Give me an example of a time when you set a goal and were able to meet or achieve it",
+                         "Tell me about a time you had to quickly adjust your work priorities to meet changing demands.",
+                         "Give me an example of a time you faced a conflict while working on a team. How did you handle that?"
+                         ]
 
 OPENAI_PROMPT = "I will be giving you workplace situation questions and I want you to evaluate my response. At the start of the sentence provide me a 1 or 0. 1 as in if my response to the situation was good and 0 if my response to the satiation is bad. This is the first question. Situation: "
-CURRENT_QUESTION = None
 
-def random_behavioural_questions() -> str:
-      return random.choice(behavioural_questions)
 
-def get_user_response() -> str:
-      #FIXME: figure out how to get user's response from frontend
-      pass
 
-def openAI_response():
+def random_behavioural_questions() -> int:
+      # ID(index) of the the random question and it's content
+      return random.randint(len(behavioural_questions))
+
+def openAI_response(user_response, question):
       #system's instruction to OpenAI
-      system_instruction = OPENAI_PROMPT + CURRENT_QUESTION
-
-      #user's response to question
-      user_response = get_user_response()
+      system_instruction = OPENAI_PROMPT + question
       
       #build message that is part of the request to OpenAI's API
       message = [{
@@ -54,20 +54,26 @@ def openAI_response():
       #OpenAI's response in sentence
       response_content = response['choices'][0]['message']['content'][3::] #double check the slicing
 
-      #retun info as tuple
-      return response_quality, response_content
+      #retun info as dict
+      return {"response_quality": response_quality, "response_content": response_content}
     
 
-      
-def controller():
-      #control flow of the program
-      pass
+@app.route('/getQuestion')
+def send_question():
+      #sending the random question from the question back to the frontend
+      current_question_ID = random_behavioural_questions()
+      return {"questionId": current_question_ID, "question":behavioural_questions[current_question_ID]}
 
-    
+@app.route("/getOpenAIResponse", methods=["POST"], strict_slashes=False)
+def receive_user_behavioural_response():
+      #questionID on display on the frontend
+      questionID = request.json["questionId"]
+      #getting the user response from the prompt
+      userResponse = request.json["userResponse"]
+      #finding the question in our question back that matches the displayed question
+      question = behavioural_questions[questionID]
+      return openAI_response(userResponse, question)
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port=8000, debug=False)
-    
-
-    
-    
